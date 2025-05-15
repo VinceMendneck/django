1a5b2e6af9c2e50b504148235d5a291c9dedf9f4
Vince Mendneck <viniciusmendonca1234@gmail.com>
2025-05-15 11:27:17 -0300

    configurando django admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_on', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'created_on'
+    ordering = ['status', 'created_on']
+
+admin.site.register(Post, PostAdmin)
